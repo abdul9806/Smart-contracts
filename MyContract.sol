5adc440cd49f54bf2caf1f93a06d47a810590181
abdul9806 <137824533+abdul9806@users.noreply.github.com>
2025-01-22 10:40:43 +0700

    Add MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -1,1 +1,13 @@
+pragma solidity ^0.8.0;
 
+contract MyContract {
+    string public message;
+
+    constructor(string memory _message) {
+        message = _message; // Inisialisasi pesan awal
+    }
+
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage; // Mengubah pesan
+    }
+}
